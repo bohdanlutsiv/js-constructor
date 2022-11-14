5d337744da4701c137e4f68ef3993463f9b2dfc0
Bohdan Lutsiv <BohdanLutsiv@gmail.com>
2022-11-14 23:17:50 +0000

    added js

---
+++ src/model.js
@@ -0,0 +1,10 @@
+export const model = [
+    {type: 'title', velue: 'Hello World from JS!!!!'},
+    {type: 'text', velue: 'here we go with same text'},
+    {type: 'columns', velue: [
+        '111111',
+        '222222',
+        '333333'
+    ]},
+{type: 'image', velue: './assets/image.png'}
+]
