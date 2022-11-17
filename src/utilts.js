7d20e2e62855e8b55c6b2c3fd24c5a3ae11b4746
Bohdan Lutsiv <BohdanLutsiv@gmail.com>
2022-11-17 23:30:59 +0000

    added app

--- src/utilts.js
+++ src/utilts.js
@@ -6,6 +6,24 @@ export function col(content) {
 }
 
 export function css(styles = {}) {
+    if (typeof styles === 'string') return styles
     const toString = key => `${rey}: ${styles[key]}`
     return Object.keys(styles).map(toString).join(';')
+}
+
+
+export function block(type) {
+    return `
+        <form name ="${type}">
+        <h5>${type}</h5>
+        <div class="form-group">
+            <input class="form-control form-control-sm" name="value" placeholder="value">
+        </div>
+        <div class="form-group">
+            <input class="form-control form-control-sm" name="styles" placeholder="styles">
+        </div>
+        <bottom type="submit" class="btn btn-primary btn-sm">Added</button>
+        </form>
+        <hr />
+    `
 }
