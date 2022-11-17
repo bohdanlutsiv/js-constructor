d090312b6b898a9b05d3083e61cbb21cb39e41e5
Bohdan Lutsiv <BohdanLutsiv@gmail.com>
2022-11-17 21:54:57 +0000

    added site and sidebar

--- src/classes/blocks.js
+++ src/classes/blocks.js
@@ -1,7 +1,53 @@
-export class Block {
-    constructor(type, value, options) {
-        this.type = type
+class Block {
+    constructor(value, options) {
         this.value = value
         this.options = options
+    }
+
+    toHTML() {
+        throw new Error('Метод toHTML має бути реалізований')
+    }
+}
+
+export class TitleBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        const {tag = 'h1', styles} = this.options
+        return row(col(`<${tag}>${this.velue}</${tag}>`), css(styles))
+    }
+}
+
+export class ImageBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        const {imageStyles: is, alt, styles} = this.options
+        return row(`<img src = "${this.velue}" alt="${alt}" style="${css(is)}"/>`, css(styles))
+    }
+}
+
+export class ColumnsBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        const html = this.velue.map(col).join('')
+        return row(html, css(this.options.styles))
+    }
+}
+
+export class TextBlock extends Block {
+    constructor(value, options) {
+        super(value, options);
+    }
+
+    toHTML() {
+        return row(col(`<p>${this.velue}</p>`), css(this.options.styles))
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import {model} from './model'
-import {templates, title} from './templates'
 import './styles/main.css'
-const $site = document.querySelector ('#site')
+import {Site} from './classes/site'
 
-model.forEach(block => {
-    const toHTML = templates[block.type]
-    if (toHTML) {
-        $site.insertAdjacentHTML ('beforeend', toHTML(block))
-    }
-})
+const site = new Site('#site')
 
+site.render(model)
